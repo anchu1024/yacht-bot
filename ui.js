d541efbe7755296c999acc5c8e8ef4c05d13b2d0
anchu1024 <anchuyu@outlook.jp>
2026-08-10 23:54:30 +0900

    feat: add parser

---
+++ ui.js
@@ -0,0 +1,50 @@
+const COLORS = {
+    // Normal
+    black: "#011627",
+    red: "#EF5350",
+    green: "#22DA6E",
+    yellow: "#c5e478",
+    blue: "#82AAFF",
+    purple: "#C792EA",
+    cyan: "#21C7A8",
+    white: "#FFFFFF",
+    // Bright
+    brightBlack: "#575656",
+    brightRed: "#EF5350",
+    brightGreen: "#22DA6E",
+    brightYellow: "#FFEB95",
+    brightBlue: "#82AAFF",
+    brightPurple: "#C792EA",
+    brightCyan: "#7FDBCA",
+    brightWhite: "#FFFFFF",
+    // ui
+    background: "#011627",
+    foreground: "#D6DEEB",
+    cursor: "#80a4c2",
+    selection: "#1d3b53",
+};
+
+function parse(str) {
+    // 1. 最初に環境依存の「¥」をすべて「\」に統一（これで誤判定の芽を摘みます）
+    let normalized = str.replaceAll("¥", "\\");
+
+    // 2. 正規表現も「\」だけに特化させてシンプルに
+    const tag = /(^|[^\\\])\[([^\]]*(?:\\.[^\]]*)*)(?<!\\)\]/g;
+
+    // タグを HTML に置換
+    let result = normalized.replace(tag, (match, prefix, ctx) => {
+        if (ctx.startsWith("bg-")) {
+            ctx = ctx.replace("bg-", "");
+            if (Object.hasOwn(COLORS, ctx)) return prefix + `<span style="background-color:${COLORS[ctx]};">`;
+        }
+        if (Object.hasOwn(COLORS, ctx)) return prefix + `<span style="color:${COLORS[ctx]};">`;
+        if (ctx === "bold") return prefix + `<span style="font-weight:bold;">`;
+        if (ctx === "italic") return prefix + `<span style="font-style:italic;">`;
+        if (ctx === "/") return prefix + `</span>`;
+        return prefix + `<span>`;
+    });
+
+    // 3. 最後にエスケープ用の「\」を綺麗にお掃除
+    const escape = /\\([\[\]])/g;
+    return result.replace(escape, "$1");
+}
