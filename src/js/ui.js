5b07e0eaae5ac795e55e2a29a172f47cf3acdf53
anchu1024 <anchuyu@outlook.jp>
2026-08-11 12:13:59 +0900

    feat: add console UI

--- src/js/ui.js
+++ src/js/ui.js
@@ -1,3 +1,5 @@
+const consoleBody = document.querySelector("#console-main");
+
 const COLORS = {
     // Normal
     black: "#011627",
@@ -29,22 +31,33 @@ function parse(str) {
     let normalized = str.replaceAll("¥", "\\");
 
     // 2. 正規表現も「\」だけに特化させてシンプルに
-    const tag = /(^|[^\\\])\[([^\]]*(?:\\.[^\]]*)*)(?<!\\)\]/g;
+    const tag = /(?<!\\)\[((?:\\.|[^\]])*?)(?<!\\)\]/g;
 
     // タグを HTML に置換
-    let result = normalized.replace(tag, (match, prefix, ctx) => {
+    let result = normalized.replace(tag, (match, ctx) => {
+        if (!ctx) return;
         if (ctx.startsWith("bg-")) {
             ctx = ctx.replace("bg-", "");
-            if (Object.hasOwn(COLORS, ctx)) return prefix + `<span style="background-color:${COLORS[ctx]};">`;
+            if (Object.hasOwn(COLORS, ctx)) return `<span style="background-color:${COLORS[ctx]};">`;
         }
-        if (Object.hasOwn(COLORS, ctx)) return prefix + `<span style="color:${COLORS[ctx]};">`;
-        if (ctx === "bold") return prefix + `<span style="font-weight:bold;">`;
-        if (ctx === "italic") return prefix + `<span style="font-style:italic;">`;
-        if (ctx === "/") return prefix + `</span>`;
-        return prefix + `<span>`;
+        if (Object.hasOwn(COLORS, ctx)) return `<span style="color:${COLORS[ctx]};">`;
+        if (ctx === "bold") return `<span style="font-weight:bold;">`;
+        if (ctx === "italic") return `<span style="font-style:italic;">`;
+        if (ctx === "/") return `</span>`;
+        return `<span>`;
     });
 
     // 3. 最後にエスケープ用の「\」を綺麗にお掃除
     const escape = /\\([\[\]])/g;
     return result.replace(escape, "$1");
 }
+
+function log(str) {
+    const formalized = "[blue]Bot>[/] " + str;
+    const parsed = parse(formalized);
+    const element = document.createElement("div");
+    element.innerHTML = parsed;
+    consoleBody.appendChild(element);
+}
+
+log("hello world! こんにちは世界!");
